67541af6acff4150523d638c0b20d17c3d55056a
leoshli <leoshli@tencent.com>
2022-09-01 17:40:14 +0800

    feat: bucket info
    --story=869092167

--- tab_cache_server_trpc2grpc.pb.go
+++ tab_cache_server_trpc2grpc.pb.go
@@ -23,6 +23,8 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type APIServerClientV2 interface {
 	GetTabConfig(ctx context.Context, in *GetTabConfigReq, opts ...grpc.CallOption) (*GetTabConfigResp, error)
+	BatchGetExperimentBucket(ctx context.Context, in *BatchGetExperimentBucketReq, opts ...grpc.CallOption) (*BatchGetExperimentBucketResp, error)
+	BatchGetGroupBucket(ctx context.Context, in *BatchGetGroupBucketReq, opts ...grpc.CallOption) (*BatchGetGroupBucketResp, error)
 }
 
 type aPIServerClientV2 struct {
@@ -42,11 +44,31 @@ func (c *aPIServerClientV2) GetTabConfig(ctx context.Context, in *GetTabConfigRe
 	return out, nil
 }
 
+func (c *aPIServerClientV2) BatchGetExperimentBucket(ctx context.Context, in *BatchGetExperimentBucketReq, opts ...grpc.CallOption) (*BatchGetExperimentBucketResp, error) {
+	out := new(BatchGetExperimentBucketResp)
+	err := c.cc.Invoke(ctx, "/opensource.tab.cache_server.APIServer/BatchGetExperimentBucket", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *aPIServerClientV2) BatchGetGroupBucket(ctx context.Context, in *BatchGetGroupBucketReq, opts ...grpc.CallOption) (*BatchGetGroupBucketResp, error) {
+	out := new(BatchGetGroupBucketResp)
+	err := c.cc.Invoke(ctx, "/opensource.tab.cache_server.APIServer/BatchGetGroupBucket", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // APIServerServerV2 is the server API for APIServer service.
 // All implementations should embed UnimplementedAPIServerServerV2
 // for forward compatibility
 type APIServerServerV2 interface {
 	GetTabConfig(context.Context, *GetTabConfigReq, *GetTabConfigResp) error
+	BatchGetExperimentBucket(context.Context, *BatchGetExperimentBucketReq, *BatchGetExperimentBucketResp) error
+	BatchGetGroupBucket(context.Context, *BatchGetGroupBucketReq, *BatchGetGroupBucketResp) error
 }
 
 // UnimplementedAPIServerServerV2 should be embedded to have forward compatible implementations.
@@ -55,6 +77,12 @@ type UnimplementedAPIServerServerV2 struct {
 
 func (UnimplementedAPIServerServerV2) GetTabConfig(context.Context, *GetTabConfigReq, *GetTabConfigResp) error {
 	return status.Errorf(codes.Unimplemented, "method GetTabConfig not implemented")
+}
+func (UnimplementedAPIServerServerV2) BatchGetExperimentBucket(context.Context, *BatchGetExperimentBucketReq, *BatchGetExperimentBucketResp) error {
+	return status.Errorf(codes.Unimplemented, "method BatchGetExperimentBucket not implemented")
+}
+func (UnimplementedAPIServerServerV2) BatchGetGroupBucket(context.Context, *BatchGetGroupBucketReq, *BatchGetGroupBucketResp) error {
+	return status.Errorf(codes.Unimplemented, "method BatchGetGroupBucket not implemented")
 }
 
 // UnsafeAPIServerServerV2 may be embedded to opt out of forward compatibility for this service.
@@ -90,6 +118,50 @@ func _APIServer_GetTabConfig_HandlerV2(srv interface{}, ctx context.Context, dec
 	return interceptor(ctx, in, info, handler)
 }
 
+func _APIServer_BatchGetExperimentBucket_HandlerV2(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BatchGetExperimentBucketReq)
+	out := new(BatchGetExperimentBucketResp)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		err := srv.(APIServerServerV2).BatchGetExperimentBucket(ctx, in, out)
+		return out, err
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opensource.tab.cache_server.APIServer/BatchGetExperimentBucket",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		rsp := new(BatchGetExperimentBucketResp)
+		err := srv.(APIServerServerV2).BatchGetExperimentBucket(ctx, req.(*BatchGetExperimentBucketReq), rsp)
+		return rsp, err
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _APIServer_BatchGetGroupBucket_HandlerV2(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BatchGetGroupBucketReq)
+	out := new(BatchGetGroupBucketResp)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		err := srv.(APIServerServerV2).BatchGetGroupBucket(ctx, in, out)
+		return out, err
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opensource.tab.cache_server.APIServer/BatchGetGroupBucket",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		rsp := new(BatchGetGroupBucketResp)
+		err := srv.(APIServerServerV2).BatchGetGroupBucket(ctx, req.(*BatchGetGroupBucketReq), rsp)
+		return rsp, err
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // APIServer_ServiceDescV2 is the grpc.ServiceDesc for APIServer service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -100,6 +172,14 @@ var APIServer_ServiceDescV2 = grpc.ServiceDesc{
 		{
 			MethodName: "GetTabConfig",
 			Handler:    _APIServer_GetTabConfig_HandlerV2,
+		},
+		{
+			MethodName: "BatchGetExperimentBucket",
+			Handler:    _APIServer_BatchGetExperimentBucket_HandlerV2,
+		},
+		{
+			MethodName: "BatchGetGroupBucket",
+			Handler:    _APIServer_BatchGetGroupBucket_HandlerV2,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
